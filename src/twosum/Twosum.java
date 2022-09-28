3363f8f93e546502722f571c430f5e292e25028e
jonas-mika <accounts@jonassenghaas.de>
2022-09-28 20:49:50 +0200

    feat(twosum): solved twosum (java)

---
+++ src/twosum/Twosum.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Twosum {
+  public static void main(String[] args) {
+    short a, b;
+    Scanner sc = new Scanner(System.in);
+    a = sc.nextShort();
+    b = sc.nextShort();
+
+    System.out.println(a+b);
+  }
+}
